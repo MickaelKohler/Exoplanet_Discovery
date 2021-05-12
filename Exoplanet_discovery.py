5783933c4571682072f09e31882a4c6e19bc72eb
Mickael Kohler <kohler.mick@gmail.com>
2021-05-12 11:22:57 +0200

    Update Exoplanet_discovery.py

--- Exoplanet_discovery.py
+++ Exoplanet_discovery.py
@@ -592,13 +592,13 @@ elif categorie == "L'IA à l'aide des Astrophysicien":
     df_test = df_exoplanet_vf[['pl_name','discoverymethod']]
     st.dataframe(df_test)
     df_final = pd.merge(df_test, df_exoplanet_rf_2,left_index=True,right_index=True)
-    df_final = df_final[['pl_name', 'disc_year', 'predictions']]
+    df_final = df_final[['pl_name', 'disc_year', 'discoverymehod_x', 'predictions']]
     st.dataframe(df_final)
     df_final.loc[df_final['predictions']==0, 'predictions'] = 'Inhabitable'
 
     df_final.rename(columns={'pl_name':"Nom de l'Exoplanète", 'discoverymehod_x':'Méthode utilisée', 'disc_year':'Découverte', 'predictions':'Prédiction'}, inplace=True)
     st.write('résulta')
-    st.dataframe(df_final, height=600)
+    st.dataframe(df_final, height=550)
 
     expander = st.beta_expander("Explication du modèle")
     expander.markdown(
